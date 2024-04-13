71f340c1c97d181cab1c77d87bede3cd17d64239
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-04-13 20:24:21 +0900

    added movie details

--- app/[id].jsx
+++ app/[id].jsx
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, Image } from "react-native";
 
 import { fetchMovie } from '../api/movies';
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: movie,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ['movie', id],
     queryFn: () => fetchMovie(id),
   });
@@ -21,7 +25,13 @@ const MovieDetails = () => {
   }
   return (
     <View>
-      <Text style={{ fontSize: 24, fontWeight: '500' }}>{data.title}</Text>
+      <Image
+        source={{ uri: 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path }}
+        style={{ width: '100%', height: 300 }}
+      />
+      <Text style={{ fontSize: 24, fontWeight: '500', marginVertical:10 }}>{movie.title}</Text>
+      <Text style={{ fontSize: 16, fontWeight: '400' }}>{movie.overview}</Text>
+
     </View>
   );
 };
