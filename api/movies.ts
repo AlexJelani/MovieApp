7613781aef5d33e0a97b7ceb97623ef27ffd9780
jirani <jelanialexander82@gmail.com>
2024-04-13 12:54:12 +0900

    Movie List UI and Details page

--- api/movies.ts
+++ api/movies.ts
@@ -1,22 +1,40 @@
+
+
+const headers = {
+  accept: 'application/json',
+  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTJjMjBiNTVhZjAzM2RkMjFlMzVmYjAyNjBkMDc4NCIsInN1YiI6IjYxOGI1MmJmY2Y2MmNkMDA2MTc1YWEzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oo36hTqhhDJSZJ1rpr3RsODxBg3FmXycu9R3yIJvxOY'
+}
 export const fetchTopRatedMovies = async () => {
   const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
     method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTJjMjBiNTVhZjAzM2RkMjFlMzVmYjAyNjBkMDc4NCIsInN1YiI6IjYxOGI1MmJmY2Y2MmNkMDA2MTc1YWEzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oo36hTqhhDJSZJ1rpr3RsODxBg3FmXycu9R3yIJvxOY'
-    }
+     headers
   };
-  try {
-    const response = await fetch(url, options);
+  const res = await fetch(url, options);
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const json = await response.json();
-    return json.results;
-  } catch (error) {
-    console.error(error);
+  if (!res.ok) {
+    throw new Error('Failed to fetch movies');
   }
 
+  let json: any;
+  json = await res.json();
+  return json.results;
+};
+export const fetchMovie = async (id: number) => {
+
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers,
+  };
+
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch movies');
+  }
+
+  let json: any;
+  json = await res.json();
+  return json;
 };
--- app/[id].jsx
+++ app/[id].jsx
@@ -1,9 +1,16 @@
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useQuery } from "@tanstack/react-query";
+import { fetchMovie} from "../api/movies";
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
+
+    const{} = useQuery({
+      fetchKey: ["movie", id],
+      queryFn: () => fetchMovie(id),
+    })
   return (
     <View>
       <Text>MovieDetail: {id}</Text>
