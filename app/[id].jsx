b7d9c1c864d2f44786e141c4f68a86cc55882b5f
jirani <jelanialexander82@gmail.com>
2024-04-16 15:45:49 +0900

    useStatebookmark

--- app/[id].jsx
+++ app/[id].jsx
@@ -1,15 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text, ActivityIndicator, Image, Pressable } from 'react-native';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { View, Text, ActivityIndicator, Image, Pressable } from 'react-native';
-
-import { fetchMovie } from '../api/movies';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { fetchMovie } from '../api/movies';
 import { addMovieToWatchList } from '../api/watchlist';
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
   const client = useQueryClient();
+  const [isBookmarked, setIsBookmarked] = useState(false); // State to manage bookmark status
 
   const {
     data: movie,
@@ -24,6 +24,7 @@ const MovieDetails = () => {
     mutationFn: () => addMovieToWatchList(id),
     onSuccess: () => {
       client.invalidateQueries(['watchlist']);
+      setIsBookmarked(true); // Update bookmark status on successful mutation
     },
     onError: (error) => {
       console.error('Error adding movie to watchlist:', error);
@@ -31,13 +32,13 @@ const MovieDetails = () => {
     },
   });
 
-
   if (isLoading) {
-    return <ActivityIndicator/>;
+    return <ActivityIndicator />;
   }
   if (error) {
     return <Text>Failed to fetch data</Text>;
   }
+
   return (
     <View>
       <Image
@@ -46,17 +47,22 @@ const MovieDetails = () => {
       />
       <Text style={{ fontSize: 24, fontWeight: '500', marginVertical: 10 }}>{movie.title}</Text>
       <View style={{ marginVertical: 10 }}>
-        <Pressable style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}
-          onPress={() => mutate()}>
-          <FontAwesome name="bookmark-o" size={24} color="black" />
+        <Pressable
+          style={{ flexDirection: 'row', alignItems: 'center', gap: 5 }}
+          onPress={() => {
+            mutate();
+          }}>
+          <FontAwesome
+            name={isBookmarked ? 'bookmark' : 'bookmark-o'} // Change icon based on bookmark status
+            size={24}
+            color={isBookmarked ? 'red' : 'black'} // Change color based on bookmark status
+          />
           <Text>Add to watchlist</Text>
         </Pressable>
       </View>
       <Text style={{ fontSize: 16, fontWeight: '400' }}>{movie.overview}</Text>
-
     </View>
   );
 };
 
 export default MovieDetails;
-
